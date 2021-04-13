082936ee6b5274c141da4ba3c70f0a85455c33a8
Blejn <blejntypie@gmail.com>
2021-04-13 15:42:39 +0200

    Nie skonczony customer

---
+++ src/main/java/projekt/customers/Customer.java
@@ -0,0 +1,77 @@
+package projekt.customers;
+
+public class Customer {
+
+   private String firstName;
+   private String lastName;
+   private String email;
+   private  String voivodeship;
+   private String city;
+   private String street;
+   private int number;
+   private int postal_code;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getVoivodeship() {
+        return voivodeship;
+    }
+
+    public void setVoivodeship(String voivodeship) {
+        this.voivodeship = voivodeship;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getPostal_code() {
+        return postal_code;
+    }
+
+    public void setPostal_code(int postal_code) {
+        this.postal_code = postal_code;
+    }
+}
---
+++ src/main/java/projekt/customers/CustomerController.java
@@ -0,0 +1,9 @@
+package projekt.customers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+}
